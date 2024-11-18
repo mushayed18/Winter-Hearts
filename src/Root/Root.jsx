3191f057ebc1701912f7fad34ed59a1c26f0cb8e
mushayed125 <kmr18adi@gmail.com>
2024-11-18 20:32:13 +0600

    project setup done

---
+++ src/Root/Root.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+const Root = () => {
+    return (
+        <div className="font-poppins">
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import './App.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root/Root';
+import ErrorPage from './Pages/ErrorPage';
+import Home from './Pages/Home';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
